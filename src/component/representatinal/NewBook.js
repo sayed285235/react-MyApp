2fd929265892323fdb65900163cd2b091a5e4cad
sayed285235 <48875366+sayed285235@users.noreply.github.com>
2021-07-29 22:53:40 +0600

    from add

--- src/component/representatinal/NewBook.js
+++ src/component/representatinal/NewBook.js
@@ -1,15 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Card, CardHeader, CardBody, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 
+class NewBook extends Component {
 
-const NewBook = props =>  {
-    // console.log(props);
+    constructor(props){
+        super(props);
 
-    return(
-        <div>
-            <p>New Book Entry</p>
-        </div>
-    );
+        this.state = {
+            bookName:"",
+            writer:"",
+            description:""
+        }
+    }
+
+    handleInputChange = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState({
+            [name]: value
+        });
+    }
+    
+    componentDidUpdate(){
+        console.log(this.state);
+    }
+
+    render(){
+        return(
+            <div>
+                <div className="col-md-5 m-auto">
+                    <Card style={{ textAlign:"start"}}>
+                        <CardHeader>
+                            New Book Entry
+                        </CardHeader>
+                        <CardBody>
+                            <Form>
+                                <FormGroup>
+                                    <Label for="bookName">BookName</Label>
+                                    <Input type="text" name="bookName" id="bookName" placeholder="BookName" 
+                                        value={this.state.bookName} onChange={(event)=> this.handleInputChange(event)}/>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="writer">Writer</Label>
+                                    <Input type="text" name="writer" id="writer" placeholder="Writer" 
+                                    value={this.state.writer} onChange={(event)=> this.handleInputChange(event)}/>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleText">Descriptions</Label>
+                                    <Input type="textarea" name="description" id="description" placeholder="Description"  
+                                    value={this.state.description} onChange={(event)=> this.handleInputChange(event)}/>
+                                </FormGroup>
+    
+                                <Button type="Submit" value="submit">Submit</Button>
+                            </Form>
+                        </CardBody>
+                    </Card>
+                </div>
+            </div>
+        );
+    }
 }
 
 
