bdd08639801e237f441e9a1a7c74a15fa3736ef6
sayed285235 <48875366+sayed285235@users.noreply.github.com>
2021-07-31 11:35:56 +0600

    update

--- src/component/representatinal/NewBook.js
+++ src/component/representatinal/NewBook.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Card, CardHeader, CardBody, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 
+
+//controlled Component From
+
 class NewBook extends Component {
 
     constructor(props){
@@ -67,5 +70,69 @@ class NewBook extends Component {
     }
 }
 
+
+
+
+/*
+
+
+//Uncontrolled Component From
+
+class NewBook extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.bookName = React.createRef();
+        this.writer = React.createRef();
+        this.description = React.createRef();
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit = event => {
+        console.log(this.bookName);
+        console.log(this.writer);
+        console.log(this.description);
+
+        event.preventDefault();
+    }
+
+    render(){
+        return(
+            <div>
+                <div className="col-md-5 m-auto">
+                    <Card style={{ textAlign:"start"}}>
+                        <CardHeader>
+                            New Book Entry
+                        </CardHeader>
+                        <CardBody>
+                            <Form onSubmit={this.handleSubmit}>
+                                <FormGroup>
+                                    <Label for="bookName">BookName</Label>
+                                    <Input type="text" name="bookName" placeholder="BookName"
+                                    ref={this.bookName} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="writer">Writer</Label>
+                                    <Input type="text" name="writer" placeholder="Writer"
+                                    ref={this.writer} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="exampleText">Descriptions</Label>
+                                    <Input type="textarea" name="description" placeholder="Description"
+                                    ref={this.description}/>
+                                </FormGroup>
+    
+                                <Button type="Submit" value="submit">Submit</Button>
+                            </Form>
+                        </CardBody>
+                    </Card>
+                </div>
+            </div>
+        );
+    }
+}
+*/
 
 export default NewBook;
